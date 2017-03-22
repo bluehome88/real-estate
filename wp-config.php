b47c656f38ca967c77f118974fedc7aaa1ae4a99
Arthur Gavrilov <artnikbrothers@gmail.com>
2017-03-22 16:25:07 +0200

    Fix permissions

--- wp-config.php
+++ wp-config.php
@@ -80,6 +80,9 @@ $table_prefix  = 'wp_';
  */
 @define('WP_DEBUG', false);
 
+// For Real estate manager plugin
+@define('FS_METHOD', 'direct');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
