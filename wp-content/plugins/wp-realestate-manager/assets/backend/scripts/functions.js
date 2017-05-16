64cb91df44a2f4c5f1d75498d738d995f3788927
Arthur Gavrilov <artnikbrothers@gmail.com>
2017-05-16 18:50:57 +0300

    Remove property expiration

--- wp-content/plugins/wp-realestate-manager/assets/backend/scripts/functions.js
+++ wp-content/plugins/wp-realestate-manager/assets/backend/scripts/functions.js
@@ -3107,3 +3107,8 @@ function wp_rem_load_dropdown_values( field_class, field_id, action ){
     });
 }
 
+jQuery(function ($) {
+    var wp_rem_property_expired_ = $('#wp_rem_property_expired');
+    wp_rem_property_expired_.val('01-01-2040');
+    wp_rem_property_expired_.closest('.form-elements').hide();
+});
