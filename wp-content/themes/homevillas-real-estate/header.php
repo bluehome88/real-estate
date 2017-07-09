7a7292c9d239021f1b03a1ad790b10ac94611823
Arthur Gavrilov <artnikbrothers@gmail.com>
2017-07-09 15:59:40 +0300

    Add GA code

--- wp-content/themes/homevillas-real-estate/header.php
+++ wp-content/themes/homevillas-real-estate/header.php
@@ -42,6 +42,16 @@ $metakeywords = cetGetProductMetaKeywords(@$post->ID, @$post->post_title);
     }
     wp_head();
     ?>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-102190640-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 </head>
 <body <?php body_class(); ?>>
     <div class="wrapper wrapper-<?php echo esc_html( $wp_rem_cs_var_layout ); ?>" <?php echo esc_html($style_bgcol); ?>>
