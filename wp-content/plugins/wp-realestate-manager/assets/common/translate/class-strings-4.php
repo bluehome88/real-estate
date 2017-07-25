b15f52c1040af3a68f4e63c9f00ae58cc227218c
Volodymyr <kinolanka@gmail.com>
2017-07-25 18:17:32 +0300

    updage changing "price on request" string on "call for price"

--- wp-content/plugins/wp-realestate-manager/assets/common/translate/class-strings-4.php
+++ wp-content/plugins/wp-realestate-manager/assets/common/translate/class-strings-4.php
@@ -432,7 +432,7 @@ if ( ! class_exists('wp_rem_plugin_all_strings_4') ) {
             $wp_rem_static_text['wp_rem_gallery_all_photos'] = esc_html__('View all photos', 'wp-rem');
 // Start Frontend Nearby Properties Elements.
             $wp_rem_static_text['wp_rem_nearby_properties_heading'] = esc_html__('Properties in the Market Nearby', 'wp-rem');
-            $wp_rem_static_text['wp_rem_nearby_properties_price_on_request'] = esc_html__('Price On Request', 'wp-rem');
+            $wp_rem_static_text['wp_rem_nearby_properties_price_on_request'] = esc_html__('Call for Price', 'wp-rem');
 // Start Frontend Opening Hours Elements.
             $wp_rem_static_text['wp_rem_opening_hours_open'] = esc_html__('Open', 'wp-rem');
             $wp_rem_static_text['wp_rem_opening_hours_today_closed'] = esc_html__('Today : Closed', 'wp-rem');
