b3ddc5e43bc92ac0d3ff2c51852bc45b10a49806
Arthur Gavrilov <artnikbrothers@gmail.com>
2017-08-16 11:34:45 +0300

    Add Google Tag Manager

--- wp-content/themes/homevillas-real-estate/header.php
+++ wp-content/themes/homevillas-real-estate/header.php
@@ -52,8 +52,19 @@ $metakeywords = cetGetProductMetaKeywords(@$post->ID, @$post->post_title);
       ga('send', 'pageview');
 
     </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MP6R2LK');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body <?php body_class(); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MP6R2LK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div class="wrapper wrapper-<?php echo esc_html( $wp_rem_cs_var_layout ); ?>" <?php echo esc_html($style_bgcol); ?>>
         <!-- Side Menu Start -->
         <div id="overlay"></div>
